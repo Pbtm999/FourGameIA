7792ad1b894aa41b5802e08f474a93a9ab55002b
Pbtm999 <pedro.batista.miguel9@gmail.com>
2024-02-21 12:25:44 +0000

    Added some comments

--- interface.py
+++ interface.py
@@ -2,10 +2,10 @@ game = [['-'] * 7] * 7
 
 
 class FourGame():
-    def __init__(self):
-        self.matrix = [['-']*7] * 7 #initialize an array with 7 collums and 7 lines
+    def __init__(self): #class constructor in python (there is no need to declare the attributes since the self.attribute do it for us)
+        self.matrix = [['-']*7] * 7 #initialize an array of arrays who represents a matrix with 7 collums and 7 lines
 
-    def __str__(self):    
+    def __str__(self): #returns the string who graphicly represents the matrix of the game
         str = ""
         for line in self.matrix: #iterate through the collums
             for sign in line: #iterate through the lines
