64f63131478f51783a8f90c37a08a7c9db57cc72
Pbtm999 <pedro.batista.miguel9@gmail.com>
2024-02-21 12:03:08 +0000

    New FourGame class and is contructor and str methods

--- interface.py
+++ interface.py
@@ -1,11 +1,18 @@
-game = [['-'] * 7] * 7 #initialize an array with 7 collums and 7 lines
+game = [['-'] * 7] * 7 
 
-def drawInterface(game):
-    str = ""
-    for line in game: #iterate through the collums
-        for sign in line: #iterate through the lines
-            str += sign
-        print(str)
+
+class FourGame():
+    def __init__(self):
+        self.matrix = [['-']*7] * 7 #initialize an array with 7 collums and 7 lines
+
+    def __str__(self):    
         str = ""
+        for line in self.matrix: #iterate through the collums
+            for sign in line: #iterate through the lines
+                str += sign
+            str += "\n"
+        return str
+        
 
-drawInterface(game)
+game = FourGame()
+print(game)
