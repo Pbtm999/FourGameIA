94c8af0d735cd720691bad823a24499155020839
Diogo Manim <d.manim.scp@gmail.com>
2024-03-29 19:48:33 +0000

    Node

--- node.py
+++ node.py
@@ -3,6 +3,8 @@ class Node():
     def __init__(self, state, parent):
         self.state = state
         self.parent = parent
+        self.N = 0
+        self.Q = 0
     
     def setPathCost(self, cost):
         self.pathCost = cost
